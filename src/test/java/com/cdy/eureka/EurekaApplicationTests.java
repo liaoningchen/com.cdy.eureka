33573dcb29c62cea4c80985637d6452f23017521
chendeyou <chendeyou@daojia-inc.com>
2020-03-09 00:32:06 +0800

    Initial commit

---
+++ src/test/java/com/cdy/eureka/EurekaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cdy.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
